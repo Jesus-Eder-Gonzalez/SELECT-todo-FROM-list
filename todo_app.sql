7fb01839a2ce1ba12c99ccf2c8e8fcec257192ac
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-07-30 16:24:40 -1000

    Completed the sixth part of this assignment.

--- todo_app.sql
+++ todo_app.sql
@@ -1,9 +1,7 @@
-BEGIN;
-DROP TABLE IF EXISTS todo_app;
-END;
+DROP DATABASE IF EXISTS todo_app;
 
 BEGIN;
-DELETE FROM pg_user WHERE EXISTS (SELECT usename FROM pg_user WHERE usename=michael);
+DROP USER IF EXISTS michael;
 END;
 
 BEGIN;
@@ -12,3 +10,13 @@ END;
 
 CREATE DATABASE todo_app;
 
+\c todo_app;
+
+CREATE TABLE tasks (
+  id integer PRIMARY KEY NOT NULL,
+  title varchar(255) NOT NULL,
+  description text,
+  created_at timestamp DEFAULT now() NOT NULL,
+  updated_at timestamp,
+  completed boolean DEFAULT false NOT NULL
+);
