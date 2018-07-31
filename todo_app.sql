44cd4bc7901821457a8a5b9477d1f2c2cf84b551
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-07-30 18:59:35 -1000

    Completed the challenge.

--- todo_app.sql
+++ todo_app.sql
@@ -23,10 +23,28 @@ ALTER TABLE tasks
   ALTER COLUMN updated_at SET NOT NULL,
   ALTER COLUMN updated_at SET DEFAULT now();
 
-INSERT INTO tasks VALUES (DEFAULT, 'Study SQL', 'Complete this exercise', DEFAULT, DEFAULT, DEFAULT);
-INSERT INTO tasks VALUES (DEFAULT, 'Study PostgreSQL', 'Read all the documentation', DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO tasks VALUES (DEFAULT, 'Study SQL', 'Complete this exercise');
+INSERT INTO tasks VALUES (DEFAULT, 'Study PostgreSQL', 'Read all the documentation');
 
 SELECT title FROM tasks WHERE completed_at IS NULL;
 
 UPDATE tasks SET completed_at = now() WHERE title='Study SQL';
+
+SELECT title, description FROM tasks WHERE completed_at IS NULL;
+
+SELECT * FROM tasks ORDER BY created_at DESC;
+
+INSERT INTO tasks VALUES (DEFAULT, 'mistake 1', 'a test entry');
+INSERT INTO tasks VALUES (DEFAULT, 'mistake 2', 'another test entry');
+INSERT INTO tasks VALUES (DEFAULT, 'third mistake', 'another test entry');
+
+SELECT title FROM tasks WHERE title LIKE '%mistake%';
+
+DELETE FROM tasks WHERE title = 'mistake 1';
+
+SELECT title FROM tasks WHERE title LIKE '%mistake%';
+
+DELETE FROM tasks WHERE title LIKE '%mistake%';
+
+SELECT * FROM tasks ORDER BY title ASC;
 
