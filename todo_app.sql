cfd4c6503788f66e5fd7d636efc41df6729587a2
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-07-30 18:33:50 -1000

    Completed task 15.

--- todo_app.sql
+++ todo_app.sql
@@ -1,18 +1,13 @@
 DROP DATABASE IF EXISTS todo_app;
 
-BEGIN;
 DROP USER IF EXISTS michael;
-END;
 
-BEGIN;
 CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker';
-END;
 
 CREATE DATABASE todo_app;
 
 \c todo_app;
 
-BEGIN;
 CREATE TABLE tasks (
   id SERIAL PRIMARY KEY NOT NULL,
   title VARCHAR(255) NOT NULL,
@@ -21,17 +16,17 @@ CREATE TABLE tasks (
   updated_at TIMESTAMP,
   completed BOOLEAN DEFAULT false NOT NULL
 );
-END;
 
-BEGIN;
 ALTER TABLE tasks 
   DROP COLUMN completed,
   ADD COLUMN completed_at TIMESTAMP DEFAULT NULL,
   ALTER COLUMN updated_at SET NOT NULL,
   ALTER COLUMN updated_at SET DEFAULT now();
-END;
 
-BEGIN;
 INSERT INTO tasks VALUES (DEFAULT, 'Study SQL', 'Complete this exercise', DEFAULT, DEFAULT, DEFAULT);
 INSERT INTO tasks VALUES (DEFAULT, 'Study PostgreSQL', 'Read all the documentation', DEFAULT, DEFAULT, DEFAULT);
-END;
+
+SELECT title FROM tasks WHERE completed_at IS NULL;
+
+UPDATE tasks SET completed_at = now() WHERE title='Study SQL';
+
